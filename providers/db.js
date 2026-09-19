ab22e6a718b08db1ceede2f7eca0a2aeef22152e
SillyTransGirl <315052621+SillyTransGirl@users.noreply.github.com>
2026-09-19 12:44:15 +0000

    fix: link DB offers to /angebotsauswahl instead of /suche

--- providers/db.js
+++ providers/db.js
@@ -169,7 +169,7 @@ function parseVendoOffers(vendoOffers, fromStation, toStation, whenDate) {
     const to = encodeURIComponent(toStation);
     const soid = encodeURIComponent(`O=${fromStation}`);
     const zoid = encodeURIComponent(`O=${toStation}`);
-    const url = `https://www.bahn.de/buchung/fahrplan/suche#sts=true&so=${from}&zo=${to}&soid=${soid}&zoid=${zoid}&kl=2&hd=${dateStr}T${pad(whenDate.getHours())}:${pad(whenDate.getMinutes())}:00&hza=D&ar=false&s=false&d=false&hz=%5B%5D&fm=false&bp=false`;
+    const url = `https://www.bahn.de/buchung/fahrplan/angebotsauswahl#sts=true&so=${from}&zo=${to}&soid=${soid}&zoid=${zoid}&kl=2&hd=${dateStr}T${pad(whenDate.getHours())}:${pad(whenDate.getMinutes())}:00&hza=D&ar=false&s=false&d=false&hz=%5B%5D&fm=false&bp=false`;
     offers.push(offer({
       provider: 'db', providerLabel: 'DB',
       operator: product.startsWith('ICE') || product.startsWith('EC') || product.startsWith('IC') ? 'Deutsche Bahn' : product,
